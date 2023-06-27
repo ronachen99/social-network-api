a22a4e9ca897013c8655a636dae39291782c5b32
Rona C <rona.chen@hotmail.com>
2023-06-26 20:00:40 -0400

    added users model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please fill a valid email address!",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
